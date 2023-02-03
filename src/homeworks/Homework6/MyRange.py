112a972d77676882419787b2badebc7e34d5b8aa
Magdas10 <111056265+Magdas10@users.noreply.github.com>
2023-02-03 15:36:55 +0400

    Update MyRange.py

--- src/homeworks/Homework6/MyRange.py
+++ src/homeworks/Homework6/MyRange.py
@@ -6,8 +6,6 @@ class MyRange:
     curr = None
 
     def __init__(self, current, end, step):
-        if current < end and step < 0 or current > end and step > 0 or current == end and step != 0:
-            raise RangeError
         if isinstance(current, int) and isinstance(end, int) and isinstance(step, int):
             self.__current = current
             self.__end = end
@@ -56,43 +54,36 @@ class MyRange:
         return self
 
     def __next__(self):
-        if self.__step == 0:
-            print(self.__current)
+        if (self.__step >= 0 and MyRange.curr >= self.end) or (self.__step < 0 and MyRange.curr < self.end):
             raise StopIteration
-        elif self.__step > 0 and self.__current > self.__end:
-            raise StopIteration
-        elif self.__step < 0 and self.__current < self.__end:
-            raise StopIteration
-        self.__current += self.__step
-        return self.__current - self.__step
+        else:
+            MyRange.curr += self.__step
+        return MyRange.curr - self.__step
 
     def __len__(self):
-        return ((self.__end - self.__current) // self.__step) + 1
+        return (self.__end - self.__current) // self.__step
 
     def __getitem__(self, item):
-        if type(item) != int or item < 0 or item >= len(self):
-            raise RangeError("Index out of range.")
-        curr = self.__current
-        curr_ind = 0
-        for num in self:
-            if curr_ind == item:
-                self.__current = curr
-                return num
-            curr_ind += 1
+        if item > len(self):
+            raise RangeError("Index out of range")
+        x = MyRange.curr
+        for _ in MyRange(1, item, 1):
+            x += self.__step
+        return x
 
     def __reversed__(self):
         return MyRange(self.__end - 1, self.__current, - self.__step)
 
 
-r = MyRange(1, 10, 2)
-print(r)
-print(r[4])
-print(len(r))
-# for i in r:
-#     print(i)
+r = MyRange(1, 10, 1)
+# print(r)
+# print(r[4])
+# print(len(r))
+for i in r:
+    print(i)
 
 rev = reversed(r)
-# print(rev)
-# print(len(rev))
-# for i in rev:
-#     print(i)
+print(rev)
+print(len(rev))
+for i in rev:
+    print(i)
